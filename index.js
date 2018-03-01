f0e479973e261c1948b9521d338ce84b059f5e99
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-03-01 17:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+var katzDeliLine = [];
+
 function takeANumber(katzDeliLine, name) {
-  
+  return `Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`; 
 }
+
+function nowServing ()
